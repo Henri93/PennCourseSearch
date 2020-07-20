7675b90c324dc5e3467936425d87a75a7977c265
Henri93 <henrygar@seas.upenn.edu>
2020-07-20 16:06:46 -0400

    add to heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 import bodyParser from 'body-parser';
 import logger from 'morgan';
-import Trie from '../client/build/trie.js';
+import Trie from '../client/build/static/trie.js';
 
 // create our instances
 const dotenv = require('dotenv');
