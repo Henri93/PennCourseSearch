f63b60c67f49cea47ba4347898771ecd6f09fd7c
Henri93 <henrygar@seas.upenn.edu>
2020-07-25 15:40:29 -0400

    added static build

---
+++ client/build/precache-manifest.9512bf860e9078cbe69416b79dfe2b37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3208cef48a16ab3cef4ff0a7535960ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92a0622539a05885371e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5f5678ef736973971f75",
+    "url": "/static/css/main.4c729085.chunk.css"
+  },
+  {
+    "revision": "92a0622539a05885371e",
+    "url": "/static/js/2.de5a4000.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.de5a4000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f5678ef736973971f75",
+    "url": "/static/js/main.b9e578f0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
